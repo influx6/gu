2fd42f229138622a25e2b9126bf639a8408f425d
Alexander Ewetumo <trinoxf@gmail.com>
2017-04-09 20:16:34 +0100

    Fix issue with base call

--- cmd/gu/main.go
+++ cmd/gu/main.go
@@ -756,7 +756,7 @@ func findLowerByStat(root string, path string, dirName string, dirOnly bool) (st
 			return findLower(filepath.Join(path, ".."), dirName)
 		}
 
-		return possiblePath, nil
+		return path, nil
 	}
 
 	return findLowerByStat(root, filepath.Join(path, ".."), dirName, dirOnly)
